6ae1ce4d60f7917d4012a811b00d1544da934823
Diney Oliveira <diney.oliveira>
2023-06-22 12:16:14 -0300

    criando a pagina de login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
+export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo do sistema de chamados' />
+                </div>
+
+                <form>
+                    <h1>Entrar</h1>
+                    <input
+                        type='text'
+                        placeholder='email@gmail.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type='password'
+                        placeholder='**********'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+
+                    />
+
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+
+
+
+            </div>
+        </div>
+    )
+}
