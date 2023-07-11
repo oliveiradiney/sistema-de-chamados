18b7ac653cbd6bd9a5c63e28abbe145983c23bf5
Diney oliveira <dineyoliveira1999@gmail.com>
2023-07-11 10:57:42 -0300

    criando o perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,20 +1,67 @@
-import Header from "../../components/Header"
-import Title from "../../components/Title"
+import Header from "../../components/Header";
+import Title from "../../components/Title";
 
-import {FiSettings} from "react-icons/fi"
+import { FiSettings, FiUpload } from "react-icons/fi";
+import avatar from "../../assets/avatar.png";
 
-export default function Profile(){
-    return (
-        <div>
-            <Header />
+import { AuthContext } from "../../contexts/auth";
+import { useContext, useState } from "react";
 
-            <div className="content">
-                <Title name='Minha conta'>
-                    <FiSettings size={25} />
-                </Title>
-            </div>
+import "./profile.css";
 
-            <h1>Pagina perfil</h1>
+export default function Profile() {
+  const { user } = useContext(AuthContext);
+
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
+  return (
+    <div>
+      <Header />
+
+      <div className="content">
+        <Title name="Minha conta">
+          <FiSettings size={25} />
+        </Title>
+
+        <div className="container">
+          <form className="form-profile">
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#FFF" size={25} />
+              </span>
+              <input type="file" accept="image/*" /> <br />
+              {avatarUrl === null ? (
+                <img
+                  src={avatar}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              ) : (
+                <img
+                  src={avatarUrl}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+
+            <label>Nome</label>
+            <input type="text" placeholder="Seu nome" />
+
+            <label>Email</label>
+            <input type="text" placeholder="teste@gmail.com" disabled={true} />
+
+            <button type="submit">Salvar</button>
+          </form>
         </div>
-    )
+
+        <div className="container">
+         <button className="logout-btn">Sair</button>
+        </div>
+
+      </div>
+    </div>
+  );
 }
