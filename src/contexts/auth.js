a8cf10cd14f46a0fb958774ab49808215d8c296c
Diney oliveira <dineyoliveira1999@gmail.com>
2024-02-09 14:54:51 -0300

    feat: implementando a pagina de criar novo chamado

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -13,15 +13,15 @@ function AuthProvider({ children }) {
 
     const [user, setUser] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false);
-    const [ loading, setLoading ] = useState(true);
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
 
     useEffect(() => {
-        async function loadUser(){
+        async function loadUser() {
             const storageUser = localStorage.getItem("@ticketsPro")
 
-            if(storageUser){
+            if (storageUser) {
                 setUser(JSON.parse(storageUser))
                 setLoading(false);
             }
@@ -36,31 +36,31 @@ function AuthProvider({ children }) {
         setLoadingAuth(true);
 
         await signInWithEmailAndPassword(auth, email, password)
-        .then(async (value) => {
-            let uid = value.user.uid;
+            .then(async (value) => {
+                let uid = value.user.uid;
 
-            const docRef = doc(db, "users", uid);
-            const docSnap = await getDoc(docRef)
+                const docRef = doc(db, "users", uid);
+                const docSnap = await getDoc(docRef)
 
 
-            let data = {
-                uid: uid,
-                nome: docSnap.data().nome,
-                email: value.user.email,
-                avatarUrl: docSnap.data().avatarUrl
-            }
+                let data = {
+                    uid: uid,
+                    nome: docSnap.data().nome,
+                    email: value.user.email,
+                    avatarUrl: docSnap.data().avatarUrl
+                }
 
-            setUser(data);
-            storageUser(data);
-            setLoadingAuth(false);
-            toast.success("Bem vindo(a) de volta!")
-            navigate("/dashboard")
-        })
-        .catch((error) => {
-            console.log(error);
-            setLoadingAuth(false);
-            toast.error("Erro ao realizar login")
-        })
+                setUser(data);
+                storageUser(data);
+                setLoadingAuth(false);
+                toast.success("Bem vindo(a) de volta!")
+                navigate("/dashboard")
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoadingAuth(false);
+                toast.error("Erro ao realizar login")
+            })
     }
 
     async function signUp(email, password, name) {
@@ -102,11 +102,11 @@ function AuthProvider({ children }) {
             })
     }
 
-    function storageUser(data){
+    function storageUser(data) {
         localStorage.setItem('@ticketsPro', JSON.stringify(data))
     }
 
-    async function logout(){
+    async function logout() {
         await signOut(auth);
         localStorage.removeItem('@ticketsPro');
         setUser(null);
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,66 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+
+import './dashboard.css';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
 
-    async function handleLogout(){
+    async function handleLogout() {
         await logout();
     }
 
     return (
         <div>
             <Header />
-            <h1>Pagina Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
-        </div>        
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        Novo chamado
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Mercado Esquina</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: "#999" }}>
+                                        Em Aberto
+                                    </span>
+                                </td>
+                                <td data-label="Cadastrado">12/05/2022</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch color="#FFF" size={17} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color="#FFF" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+            </div>
+        </div>
     );
 }
