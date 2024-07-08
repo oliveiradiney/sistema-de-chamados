284755322c1fc08ffafd2ab8e6236c60509ed939
Diney oliveira <dineyoliveira1999@gmail.com>
2024-07-08 14:45:54 -0300

    feat: passando clientes no select

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -2,8 +2,70 @@ import { FiPlusCircle } from "react-icons/fi";
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import './new.css'
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+import { db } from "../../services/firebaseConnection";
+import { collection, getDoc, getDocs, doc } from "firebase/firestore";
+
+
+const listRef = collection(db, 'customers')
 
 export default function New() {
+
+  const { user } = useContext(AuthContext)
+
+  const [customers, setCustomers] = useState([])
+  const [loadCustomer, setLoadCustomer] = useState(true)
+  const [customerSelected, setCustomerSelected] = useState(0)
+  const [complemento, setComplemento] = useState('')
+  const [assunto, setAssunto] = useState('Suporte')
+  const [status, setStatus] = useState('Aberto')
+
+  useEffect(() => {
+    async function loadCustomer() {
+      const querySnapshot = await getDocs(listRef)
+        .then((snapshot) => {
+          const lista = []
+
+          snapshot.forEach((doc) => {
+            lista.push({
+              id: doc.id,
+              nomeFantasia: doc.data().nomeFantasia
+            })
+          })
+
+          if (snapshot.docs.size === 0) {
+            console.log('NENHUMA EMPRESA ENCONTRADA')
+            setCustomers([{ id: '1', nomeFantasia: 'FREELA' }])
+          }
+
+          setCustomers(lista)
+          setLoadCustomer(false)
+
+        })
+        .catch((error) => {
+          console.log('ERRO AO BUSCAR OS CLIENTES', error)
+          setLoadCustomer(false)
+          setCustomers([{ id: '1', nomeFantasia: 'FREELA' }])
+        })
+
+    }
+
+    loadCustomer()
+  }, [])
+
+  function handleOptionChange(e) {
+    setStatus(e.target.value)
+  }
+
+  function handleChangeSelect(e) {
+    setAssunto(e.target.value)
+  }
+
+  function handleChangeCustomer(e) {
+    setCustomerSelected(e.target.value)
+  }
+
   return (
     <div>
       <Header />
@@ -15,13 +77,29 @@ export default function New() {
 
         <div className="container">
           <form className="form-profile">
-            <label>Cliente</label>
-            <select>
+            <label>Clientes</label>
+            {
+              loadCustomer ? (
+                <input type="text" disabled={true} value='Carregando...' />
+              ) : (
+                <select value={customerSelected} onChange={handleChangeCustomer}>
+                  {customers.map((item, index) => {
+                    return (
+                      <option key={index} value={index}>
+                        {item.nomeFantasia}
+                      </option>
+                    )
+                  })}
+                </select>
+              )
+            }
+            {/* <select>
               <option key={1} value={1}>Mercado de teste</option>
-              <option key={2} value={2}>Mercado de teste</option>
-            </select>
+              <option key={2} value={2}>Loja de informatica</option>
+            </select> */}
+
             <label>Assunto</label>
-            <select>
+            <select value={assunto} onChange={handleChangeSelect}>
               <option value="Suporte">Suporte</option>
               <option value="Visita Tecnica">Visita Tecnica</option>
               <option value="Financeiro">Financeiro</option>
@@ -32,18 +110,24 @@ export default function New() {
                 type="radio"
                 name="radio"
                 value="Aberto"
+                onChange={handleOptionChange}
+                checked={status === 'Aberto'}
               />
               <span>Em aberto</span>
               <input
                 type="radio"
                 name="radio"
                 value="Progresso"
+                onChange={handleOptionChange}
+                checked={status === 'Progresso'}
               />
               <span>Progresso</span>
               <input
                 type="radio"
                 name="radio"
                 value="Atendido"
+                onChange={handleOptionChange}
+                checked={status === 'Atendido'}
               />
               <span>Atendido</span>
             </div>
@@ -52,6 +136,8 @@ export default function New() {
             <textarea
               type="text"
               placeholder="Descreva seu problema (opcional)."
+              value={complemento}
+              onChange={(e) => setComplemento(e.target.value)}
             />
 
             <button type="submit">Registrar</button>
